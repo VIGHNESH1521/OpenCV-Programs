e2e6f78328820096ab457a76abed94d07c8027cf
VIGHNESH SUDHAKAR <90493668+VIGHNESH1521@users.noreply.github.com>
2022-01-21 20:56:35 +0530

    Add files via upload

---
+++ CameraParameters.py
@@ -0,0 +1,38 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,1208)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while (cap.isOpened()):
+
+    # Capture the video frame
+    # by frame
+    ret, frame = cap.read()
+    if ret:
+        text = "Width : " + str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) + ", Height : " + str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(frame,
+                    text,
+                    (50, 50),
+                    font, 1,
+                    (255, 0, 0),
+                    2,
+                    cv2.LINE_4)
+        cv2.imshow('frame',frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# After the loop release the cap object
+cap.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
