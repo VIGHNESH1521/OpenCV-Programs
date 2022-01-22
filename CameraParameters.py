b0b01a1127bb0ccd726fa36551195e49306dfd05
VIGHNESH SUDHAKAR <90493668+VIGHNESH1521@users.noreply.github.com>
2022-01-22 13:09:22 +0530

    Add files via upload

--- CameraParameters.py
+++ CameraParameters.py
@@ -1,11 +1,12 @@
 import cv2
+import datetime
 
 cap = cv2.VideoCapture(0)
 print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,1208)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1208)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
 
 print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -16,16 +17,19 @@ while (cap.isOpened()):
     # by frame
     ret, frame = cap.read()
     if ret:
-        text = "Width : " + str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) + ", Height : " + str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        text = "Width : " + str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) + ", Height : " + str(
+            cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        PresentDate = str(datetime.datetime.now())
+        DateText = text + " " + PresentDate
         font = cv2.FONT_HERSHEY_SIMPLEX
         cv2.putText(frame,
-                    text,
+                    DateText,
                     (50, 50),
                     font, 1,
                     (255, 0, 0),
                     2,
                     cv2.LINE_4)
-        cv2.imshow('frame',frame)
+        cv2.imshow('frame', frame)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
